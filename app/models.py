f7cdb99953618f7cc058926dd0320ec16740b779
disidro <disidro@campus.udes.edu.co>
2020-10-02 20:24:45 -0500

    Google storage

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from gdstorage.storage import GoogleDriveStorage
 
+# Define Google Drive Storage
+gd_storage = GoogleDriveStorage()
 # Create your models here.
+class Doc(models.Model):
+    name = models.CharField(max_length=200)
+    file = models.FileField(upload_to='docs', storage=gd_storage)
