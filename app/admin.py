4c9073c22d3c7e055877af41c085cbb60da95848
edwin velasco <edwinalbertovelasco8@gmail.com>
2020-12-04 14:37:55 -0500

    ajusted for register, document

--- app/admin.py
+++ app/admin.py
@@ -1,27 +1,16 @@
 from django.contrib import admin
-from app.models import UserMail, Dependence, Document
+from app.models import UserMail
 
 
 # Register your models here.
 
 
-class DocumentAdmin(admin.ModelAdmin):
-    fields = ('identification_applicant', 'name_applicant', 'email_applicant',
-              'expedition', 'file_original', 'document_type', 'enable')
-
-
 class UserMailAdmin(admin.ModelAdmin):
-    fields = ('email', 'role', 'active', 'updated', 'created','password')
+    fields = ('email', 'role', 'active', 'updated', 'created', 'password')
     readonly_fields = ('updated', 'created')
     list_display = ['email', 'role', 'active']
 
 
-class DependenceAdmin(admin.ModelAdmin):
-    fields = ('name',)
-
-
-admin.site.register(Document, DocumentAdmin)
 admin.site.register(UserMail, UserMailAdmin)
-admin.site.register(Dependence, DependenceAdmin)
 
 
